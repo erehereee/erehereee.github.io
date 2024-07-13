885400fbbf7071fd20bc27ab554db36c452413e8
Glep <skypongantung11@gmail.com>
2024-07-13 13:53:16 +0800

    Paling Updated 6

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const button = document.getElementById('hamburger-button');
 const powerList = document.getElementById('power-list');
 const powerListItem = document.getElementById('power-list-item');
+
+const energyList = document.getElementById('energy-list');
+const energyListItem = document.getElementById('energy-list-item');
+
 button.addEventListener('click', function() {
     const buttonClass = document.querySelector('.container .sidebar');
     buttonClass.classList.toggle('hide');
@@ -14,3 +18,13 @@ powerList.addEventListener('click', function() {
     powerList.classList.toggle('hide');
     powerListItem.classList.toggle('hide');
 });
+
+energyList.addEventListener('mouseleave', function() {
+    energyList.classList.add('hide');
+    energyListItem.classList.add('hide');
+});
+energyList.addEventListener('click', function() {
+    energyList.classList.toggle('hide');
+    energyListItem.classList.toggle('hide');
+});
+
