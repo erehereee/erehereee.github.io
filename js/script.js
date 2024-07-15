dcc8859376fd32295961d458f4df209dc8e4c074
Glep <skypongantung11@gmail.com>
2024-07-15 15:29:17 +0800

    Laste Di Pabrik

--- js/script.js
+++ js/script.js
@@ -3,10 +3,15 @@ const button = document.getElementById('hamburger-button');
 const energyList = document.getElementById('energy-list');
 const energyListItem = document.getElementById('energy-list-item');
 
+const phaseButton = document.getElementById('change-button');
+
+
+
+
 button.addEventListener('click', function() {
     const buttonClass = document.querySelector('.container .sidebar');
     buttonClass.classList.toggle('hide');
-})
+});
 
 energyList.addEventListener('mouseleave', function() {
     energyList.classList.add('hide');
@@ -15,5 +20,14 @@ energyList.addEventListener('mouseleave', function() {
 energyList.addEventListener('click', function() {
     energyList.classList.toggle('hide');
     energyListItem.classList.toggle('hide');
+});
+
+phaseButton.addEventListener('click', function() {
+    const buttonClassLN = document.querySelector('.container .main-content .main-dashboard .top .gauge-1phase');
+    const buttonClassLL = document.querySelector('.container .main-content .main-dashboard .top .gauge-3phase');
+    // const titleText = document.querySelector('.container .main-content .main-dashboard .top .selector .title h3');
+    buttonClassLN.classList.toggle('hide');
+    buttonClassLL.classList.toggle('hide');
+    // titleText.innerHTML = 'Voltage/Current 1 Phase';
 });
 
