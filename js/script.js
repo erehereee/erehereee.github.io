cd0935d78332b3f1994a3efa432ca65ae8ec9628
Glep <skypongantung11@gmail.com>
2024-07-15 20:53:42 +0800

    Laste ini hari

--- js/script.js
+++ js/script.js
@@ -25,9 +25,15 @@ energyList.addEventListener('click', function() {
 phaseButton.addEventListener('click', function() {
     const buttonClassLN = document.querySelector('.container .main-content .main-dashboard .top .gauge-1phase');
     const buttonClassLL = document.querySelector('.container .main-content .main-dashboard .top .gauge-3phase');
-    // const titleText = document.querySelector('.container .main-content .main-dashboard .top .selector .title h3');
+    const titleText = document.querySelector('.container .main-content .main-dashboard .top .selector .title h3');
+    const testGet1 = buttonClassLN.getAttribute('class');
     buttonClassLN.classList.toggle('hide');
     buttonClassLL.classList.toggle('hide');
-    // titleText.innerHTML = 'Voltage/Current 1 Phase';
+    if(testGet1 == "gauge-1phase hide") {
+        titleText.innerHTML = 'Voltage/Current 1 Phase';
+    }
+    else {
+        titleText.innerHTML = 'Voltage/Current 3 Phase';
+    }
 });
 
