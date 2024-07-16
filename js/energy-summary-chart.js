b6cdccede660cb269a69900095929f7e6cebd194
Glep <skypongantung11@gmail.com>
2024-07-16 13:14:22 +0800

    Update Chart.js

---
+++ js/energy-summary-chart.js
@@ -0,0 +1,20 @@
+var ctx = document.getElementById('energy-chart').getContext('2d');
+
+var chart =  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
